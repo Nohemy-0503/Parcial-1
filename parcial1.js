3d3fac1d73e30337c30d54c1a2e0358087b18fe7
Nohemy-0503 <110186577+Nohemy-0503@users.noreply.github.com>
2024-02-20 10:34:58 -0600

    Add files via upload

---
+++ parcial1.js
@@ -0,0 +1,9 @@
+function mostrarDetalles(titulo, descripcion, imagen) {
+    alert(`Detalles:\nTítulo: ${titulo}\nDescripción: ${descripcion}`);
+    // Puedes abrir una ventana modal o realizar otras acciones según tus necesidades
+  }
+  
+  function mostrarDescripcion() {
+    var descripcion = document.getElementById("descripcion");
+    descripcion.innerHTML = "La fascinante mezcla de emociones en los animes crea un viaje inolvidable, mientras que los Pokémon añaden un toque de nostalgia y aventura a la experiencia audiovisual";
+}
